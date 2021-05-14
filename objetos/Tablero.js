18a0ae07bc8e6d65fd03a14771d2edcbb1526a51
Pablo Cordero <pablocorderoromero@gmail.com>
2021-05-14 11:10:19 +0200

    🎉 Three.js project initialized

---
+++ objetos/Tablero.js
@@ -0,0 +1,14 @@
+import * as THREE from '../libs/three.module.js'
+
+class Tablero extends THREE.Object3D {
+  constructor(gui, titleGui) {
+
+
+  }
+
+  update() {
+    
+  }
+}
+
+export { Tablero };
